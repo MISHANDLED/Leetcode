cb10d8ac10e27e4ef2794795f0af7a3a04f129c6
MISHANDLED <52106838+MISHANDLED@users.noreply.github.com>
2020-04-13 20:32:36 +0530

    Update Last_Stone_Weight.py

--- Problems/Last_Stone_Weight.py
+++ Problems/Last_Stone_Weight.py
@@ -1,27 +1,21 @@
-# Runtime: 28 ms
-# Memory Usage: 13.9 MB
+# Runtime: 20 ms
+# Memory Usage: 14 MB
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         res = stones[0]
         while len(stones) > 2:
-            res = stones[0]
             x = max(stones)
             stones.remove(x)
             y = max(stones)
             stones.remove(y)
-            if x > y:
-                stones.append(x-y)
-            elif x < y:
-                stones.append(y-x)
-            else:
-                stones.append(0)
+            stones.append(abs(x-y))
+            
         if len(stones) == 2:
-            x = stones[0]
-            y = stones[-1]
-            if x > y:
-                return x - y
-            elif x < y:
-                return y - x
+            
+            if (stones[0]!=stones[1]):
+                return abs(stones[0] - stones[1])
+            
             else:
                 return 0
+        
         return res
